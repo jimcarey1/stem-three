55cbfff8e74ae8c122ee70d11f865f9328774cf1
Viswanadh <viswanadhlingareddy@gmail.com>
2025-05-17 15:19:41 +0530

    Added VideoJS, basically tested it.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 
 import GoogleBranding from './helpers/GoogleBranding';
 import Home from './components/Home';
+import VideoJS from './components/VideoJS';
 import {checkAuthStatus, updateTokens, handleLogin} from './utils/Auth';
 
 function App() {
@@ -29,9 +30,37 @@ function App() {
   // }, []);
   // if (loading) return <p>Loading...</p>;
 
+  const playerRef = useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    aspectRatio:'16:9',
+    playbackRates:[0.25, 0.5, 0.75, 1],
+    sources: [{
+      src: '/testing/testing.mp4',
+      type: 'video/mp4'
+    }]
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+      videojs.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      videojs.log('player will dispose');
+    });
+  };
+
   return (
     <>
     <Home />
+    <VideoJS options={videoJsOptions} onReady={handlePlayerReady}/>
     </>
   )
 }
